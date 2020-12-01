965dbfed7ca6fa76d40d38e73a9d8655b1d39eeb
tej0 <tejaDavuluri007@gmail.com>
2020-12-01 14:15:36 +0530

    add project page

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle:'projects'
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page',
